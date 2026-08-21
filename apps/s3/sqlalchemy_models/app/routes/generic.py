967365d048a011600b28ec7bc8ad6774cd0ae99a
paulhectork <paul.kervegan@hotmail.fr>
2026-08-22 01:33:23 +0200

    s3: app sqlalchemy models

--- apps/s3/sqlalchemy_models/app/routes/generic.py
+++ apps/s3/sqlalchemy_models/app/routes/generic.py
@@ -3,14 +3,34 @@ from sqlalchemy import text
 
 from app.app import app, db
 from app.utils.constants import APP_NAME
-from 
+from app.models.data import Iconography
 
 @app.route("/")
 def index():
-    return render_template("pages/homepage.html", app_name=APP_NAME)
+    """
+    page d'accueil
+    """
+    # on affiche les 5 premiers résultats de Icono
+    query = db.select(Iconography).order_by(Iconography.id.desc()).limit(10)
+    icono_preview = db.session.execute( query ).scalars().all() 
+    return render_template("pages/homepage.html", app_name=APP_NAME, icono_preview=icono_preview)
 
-@app.route("/iconographie")
+
+@app.route("/iconographie/")
 def icono_index():
-    result = db.session.execute(text("SELECT * FROM iconography;"))
-    icono_corpus = result.all()
+    """
+    index des ressources icono
+    """
+    # on affiche tout l'index icono: 
+    query = db.select(Iconography)
+    icono_corpus = db.session.execute( query ).scalars().all() 
     return render_template("pages/icono_index.html", app_name=APP_NAME, icono_corpus=icono_corpus)
+
+
+@app.route("/iconographie/<int:id_icono>")
+def icono_main(id_icono: int):
+    """
+    vue principale d'une ressource icono
+    """
+    icono_item = db.get_or_404(Iconography, id_icono)
+    return render_template("pages/icono_main.html", app_name=APP_NAME, icono_item=icono_item)
