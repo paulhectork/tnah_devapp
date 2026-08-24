dce7331b850666216289fc77bc8c9098e420f0ff
paulhectork <paul.kervegan@hotmail.fr>
2026-08-25 00:11:28 +0200

    s3: update app sqlalchemy_models with mapped_column pattern

--- apps/s3/sqlalchemy_models/app/models/data.py
+++ apps/s3/sqlalchemy_models/app/models/data.py
@@ -1,14 +1,22 @@
+from typing import Optional
+
+from sqlalchemy.orm import Mapped, mapped_column
+
 from app.app import db
 
+
+# comment lit-on chacun des attributs ci-dessous ?
 class Iconography(db.Model):
-    id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True, autoincrement=True)
-    title = db.Column(db.Text, nullable=False)
-    iiif_manifest_url = db.Column(db.Text, nullable=False)
-    iiif_image_url = db.Column(db.Text, nullable=False)
-    source_url = db.Column(db.Text)
-    richelieu_url = db.Column(db.Text, nullable=False)
-    date_lower = db.Column(db.Integer)    
-    date_upper = db.Column(db.Integer)
-    institution = db.Column(db.Integer, nullable=False)
+    __tablename__ = "Iconography"
+    
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    iiif_manifest_url: Mapped[str]
+    iiif_image_url: Mapped[str]
+    source_url: Mapped[Optional[str]]
+    richelieu_url: Mapped[str]
+    date_lower: Mapped[Optional[int]]
+    date_upper: Mapped[Optional[int]]
+    institution: Mapped[int]
     # NOTE: il reste à définir la relation avec la table `author`, mais on verra comment faire plus tard !
     id_author = ...
