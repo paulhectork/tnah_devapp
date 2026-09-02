b6f0b56d72dd75198f15cdb6698b9a9de83954fe
paulhectork <paul.kervegan@hotmail.fr>
2026-09-02 15:29:37 +0200

    s4: fix: add back sqlalchemy_many_to_many

---
+++ apps/s4/sqlalchemy_many_to_many/app/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from app.utils.constants import DIR_TEMPLATES, DIR_STATICS, PATH_DB, APP_NAME
+
+print(">>>", PATH_DB)
+
+app = Flask(
+    APP_NAME,
+    template_folder=DIR_TEMPLATES, 
+    static_folder=DIR_STATICS
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{PATH_DB}"
+db = SQLAlchemy(app)
+
+# from sqlalchemy import text
+# with app.app_context():
+#     print(db.session.execute(text("select * from iconography")).all())
+
+from app.routes import generic
