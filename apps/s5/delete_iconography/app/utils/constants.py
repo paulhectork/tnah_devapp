903bc1a86c29a1a1a41d38045978905952ec5b06
paulhectork <paul.kervegan@hotmail.fr>
2026-09-10 20:28:00 +0200

    s5: done !

---
+++ apps/s5/delete_iconography/app/utils/constants.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from warnings import warn
+
+APP_NAME = "Catalogue Richelieu"
+
+# chemin absolu vers dossier utils/
+DIR_UTILS = Path(__file__).parent.resolve()
+
+# chemin absolu vers dossier app/ (parent de utils/)
+DIR_APP = DIR_UTILS.parent.resolve()
+
+# chemin absolu vers la racine de l'application (parent de app/)
+DIR_ROOT = DIR_APP.parent.resolve()
+
+# chemin absolu vers notre dossier de templates (app/templates/)
+DIR_TEMPLATES = DIR_APP / "templates" 
+
+# chemin absolu vers notre dossier de statics (app/statics/)
+DIR_STATICS = DIR_APP / "statics" 
+
+# chemin vers la base de données sqlite (à la racine du dossier `tnah_devapp/`)
+PATH_DB = DIR_ROOT.parent.parent.parent.resolve() / "richelieu.db"
+
+# la clé top secrète
+secret_key_default = "Une clé secrète"
+SECRET_KEY = "Une clé secrète"
+
+if SECRET_KEY == secret_key_default:
+    warn(f"Changez votre clé secrète avant de passer en production ! Clé secrète actuelle: {SECRET_KEY}")
